ff8a5af38789b199c5d0a0406daa1cd477d4c349
Marko Boras <mboras1@etfos.hr>
2020-12-11 17:55:52 +0100

    Task 1 - Creating the API Project

---
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        public string[] Get()
+        {
+            return new string[]
+            {
+                 "Hello",
+                "World"
+            };
+        }
+    }
+}
+
